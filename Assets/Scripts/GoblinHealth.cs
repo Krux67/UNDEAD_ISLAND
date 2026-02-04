2b7e619f96836df4bf663817a666750313f54c75
DroidR117 <Luke.Martin@mrwaraksa.com>
2026-02-04 12:57:12 -0500

    Enemies

---
+++ Assets/Scripts/GoblinHealth.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GoblinHealth : MonoBehaviour
+{
+    public int health = 50;
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        Debug.Log("Goblin Health: " + health);
+        if (health <= 0) Die();
+    }
+
+    void Die()
+    {
+        Debug.Log("Goblin died!");
+        Destroy(gameObject); // Remove goblin from scene
+    }
+}
